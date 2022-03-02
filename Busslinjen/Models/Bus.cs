827fb5863423d1c74fa6ee7a82b9a0ed6b22d147
Jonas Rönnqvist <jonas.ronnqvist@iths.se>
2022-03-02 14:35:08 +0100

    xml

--- Busslinjen/Models/Bus.cs
+++ Busslinjen/Models/Bus.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace Busslinjen.Models
 {
+    /// <summary>
+    /// Bus parameters.
+    /// </summary>
     public class Bus
     {
+         /// <param name="MaxPassengers">
+        /// maximum amount of passengers
+        /// </param>
         public int MaxPassengers { get; set; } = 10;
         public int PassengersInTheBus { get; set; }
         public bool BusFull { get; set; } = false;
